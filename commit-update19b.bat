19435c547ae00e3c839dae8c7a500ac6b9d60a1b
cblais75 <cblais75@gmail.com>
2026-07-14 20:34:07 -0400

    Update 19

---
+++ commit-update19b.bat
@@ -0,0 +1,38 @@
+@echo off
+cd /d "C:\Users\colin\OneDrive\Desktop\Maine Election App\maine-needle"
+
+set "PGIT_DIR=%LOCALAPPDATA%\PortableGit"
+set "GIT_EXE=%PGIT_DIR%\bin\git.exe"
+set "GH_EXE=C:\Program Files\GitHub CLI\gh.exe"
+set "PATH=%PGIT_DIR%\bin;%PGIT_DIR%\usr\bin;%PATH%"
+set "LOG=%~dp0commit-update19b-log.txt"
+
+echo. > "%LOG%"
+echo === commit-update19b.bat === >> "%LOG%"
+
+if exist ".git\index.lock" (
+  del /q ".git\index.lock"
+  echo Removed stale index.lock >> "%LOG%"
+)
+
+"%GIT_EXE%" add -A >> "%LOG%" 2>&1
+echo Add exit: %ERRORLEVEL% >> "%LOG%"
+
+"%GIT_EXE%" commit -m "Update 19" >> "%LOG%" 2>&1
+echo Commit exit: %ERRORLEVEL% >> "%LOG%"
+
+"%GIT_EXE%" log --oneline -4 >> "%LOG%" 2>&1
+
+"%GH_EXE%" auth token > "%~dp0tok.tmp" 2>> "%LOG%"
+set /p TOKEN= < "%~dp0tok.tmp"
+del /q "%~dp0tok.tmp" 2>nul
+
+"%GIT_EXE%" remote set-url origin https://cblais75:%TOKEN%@github.com/cblais75/maine-needle.git
+"%GIT_EXE%" push origin main >> "%LOG%" 2>&1
+set "PUSH_EXIT=%ERRORLEVEL%"
+"%GIT_EXE%" remote set-url origin https://github.com/cblais75/maine-needle.git
+
+echo Push exit: %PUSH_EXIT% >> "%LOG%"
+echo === Done === >> "%LOG%"
+type "%LOG%"
+pause
